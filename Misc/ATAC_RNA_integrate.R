921f9dfdf05ffdaf650842fcb3b0b4847d3b8746
henryertl <henry.a.ertl@gmail.com>
2021-02-10 17:11:40 -0500

    cleaned up integration script

--- Misc/ATAC_RNA_integrate.R
+++ Misc/ATAC_RNA_integrate.R
@@ -1,6 +1,12 @@
 # generate bedtools closest file by finding closest gene coordinates (or first exon) OF EXPRESSED GENES IN RNA-SEQ
 ## generated input files by extracting coordinates from RNA and ATAC files (see below) and sorting by coordinate
 
+
+
+##################################
+## generating files input files ##
+##################################
+
 # attach first exon coords to RNA data
 RNA <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/RNA_seq/Data_tables/Full_results_output_ZHR_Z30_RNA_20min_100max.txt", header = T)
 first_exon_coords <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/BED_files_for_analyses/dm6_gene_coords_first_exon.bed", header = F)
@@ -9,44 +15,47 @@ RNA_coords <- join_all(list(RNA, first_exon_coords), by = "Gene", type = "left")
 write.table(RNA_coords, file = "/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/RNA_seq/Data_tables/Full_results_output_ZHR_Z30_RNA_20min_100max_exon1_coords.txt", row.names = F, quote = F, sep = "\t")
 
 # rearrange RNA_coords in unix to just be coordinates and in BED format for below
-
 bedtools closest \
 -a /Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/ATAC_seq/ZHR_Z30_Full_results_output_ALL_classes_ONLY_coords_sorted.bed \
 -b /Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/RNA_seq/Data_tables/Full_results_output_ZHR_Z30_RNA_20min_100max_coords_only_sorted.bed \
 | uniq \
 > /Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/ATAC_RNA_comp/ZHR_Z30_ATAC_RNA_closest.bed
 
+########################################
+## join files for integrated analyses ##
+########################################
+
 # read in data from bedtools
 ATAC <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/ATAC_seq/ZHR_Z30_Full_results_output_ALL_classes.txt", header = T)
 RNA <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/RNA_seq/Data_tables/Full_results_output_ZHR_Z30_RNA_20min_100max.txt", header = T)
 ATAC_RNA_closest  <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/ATAC_RNA_comp/ZHR_Z30_ATAC_RNA_closest.bed", header = F, sep = "\t")
-## clean up ATAC and RNA to only keep necessary columns
-# prepare ATAC and RNA files to merge
-RNA_minimal <- RNA[,c(1,28:40,43)]
-ATAC_minimal <- ATAC[,c(1:4,19:31,34,35)]
 
-## select classes
+## clean up ATAC data - keep relevant columns and separate classes
+ATAC_minimal <- ATAC[,c(1:4,19:31,34,35)]
 ATAC_minimal_intra_inter <-  ATAC_minimal[ATAC_minimal$class == "inter" | ATAC_minimal$class == "intra",]
 ATAC_minimal_start_end <- ATAC_minimal[ATAC_minimal$class == "start" | ATAC_minimal$class == "end",]
 
- 
+## clean up RNA data  - keep relevant columns and rename columns in prep for join with ATAC
+RNA_minimal <- RNA[,c(1,28:40,43)]
 colnames(RNA_minimal) <- paste(colnames(RNA_minimal), "RNA", sep = "_")
 colnames(RNA_minimal)[1] <- "Gene"
 
-## clean up closest file to only keep needed columnns and create a locus key
+## clean up closest file - add locus key to join with
 ATAC_RNA_closest$Paste_locus <- paste(ATAC_RNA_closest$V1, ATAC_RNA_closest$V2, ATAC_RNA_closest$V3, sep = "_")
 ATAC_RNA_closest <- ATAC_RNA_closest[,c(5,7,8)]
 colnames(ATAC_RNA_closest) <- c("beg_first_exon", "Gene", "Paste_locus")
 
-# join closest gene and exon coordinate to ATAC_inter_intra file
+# SET 1: integrate closest gene info for inter- and intra-genic ATAC regions
+## join closest gene and exon coordinate to inter- and intra-genic regions
 ATAC_minimal_intra_inter_closest_gene <- join_all(list(ATAC_minimal_intra_inter, ATAC_RNA_closest), by = "Paste_locus", type = "full") %>% unique() %>% na.omit()
-
-# join gene expression information
+## join gene expression information
 RNA_minimal_closest_locus <- join_all(list(RNA_minimal, ATAC_minimal_intra_inter_closest_gene), by = "Gene", type = "full") %>% unique() %>% na.omit()
+## OUTPUT 4410 regions with closest gene info
 
 
 
-# prepare txSTart and End files
+# Set 2: integrate start and end data with corresponding gene and expression info
+# prepare txSTart and End files - basically just re-writing the locus key
 start_end <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/BED_files_for_analyses/dm6_all_uniq", header = T)
 gene_conversions <- read.delim("/Users/henryertl/Documents/Wittkopp_lab/AS_ATAC_RNA_2020_10_1/BED_files_for_analyses/Dmel_geneID_conversion_table.txt", header = F)
 gene_conversions <- gene_conversions[,c(1,5)]
@@ -58,9 +67,7 @@ start_minimal$start_down500 <- start_minimal[,2] + 500
 start_minimal$Paste_locus <- paste(start_minimal[,1], start_minimal[,4], start_minimal[,5], sep = "_")
 start_minimal <- start_minimal[,c(6,3)]
 colnames(start_minimal) <- c("Paste_locus", "Gene")
-
 start_key <- join_all(list(start_minimal, gene_conversions), type = "left", by = "Gene")
-
 
 end_minimal <- start_end[,c(1,4,7)]
 end_minimal$end_up500 <- end_minimal[,2] - 500
@@ -68,20 +75,70 @@ end_minimal$end_down500 <- end_minimal[,2] + 500
 end_minimal$Paste_locus <- paste(end_minimal[,1], end_minimal[,4], end_minimal[,5], sep = "_")
 end_minimal <- end_minimal[,c(6,3)]
 colnames(end_minimal) <- c("Paste_locus", "Gene")
-
 end_key <- join_all(list(end_minimal, gene_conversions), type = "left", by = "Gene")
 
+#### this is the final key for txStart and txEnd coordinates and gene info
 start_end_key <- rbind(start_key, end_key)
 
 # join associated gene with paste_locus from start and end files
-
 ATAC_minimal_start_end_gene <- join_all(list(ATAC_minimal_start_end, start_end_key), type = "left", by = "Paste_locus") %>% unique() %>% na.omit()
-
 colnames(ATAC_minimal_start_end_gene)[20] <- "Name"
 colnames(ATAC_minimal_start_end_gene)[21] <- "Gene"
 
+# join gene expression info
 ATAC_minimal_start_end_gene_RNA_int <- join_all(list(RNA_minimal, ATAC_minimal_start_end_gene), type = "full", by = "Gene") %>% unique() %>% na.omit()
 ATAC_minimal_start_end_gene_RNA_int$Name <- NULL
+ATAC_minimal_start_end_gene_RNA_int$beg_first_exon <- 0
+
+## OUTPUT: 3786 txStart/End regions
+
+### join 2 classes ###
+all_classes_integrated <- rbind(ATAC_minimal_start_end_gene_RNA_int, RNA_minimal_closest_locus)
+
+### ANALYSES ###
+
+# Categorize
+
+all_classes_integrated$div_category <- "NA"
+
+for (i in 1:nrow(all_classes_integrated)) {
+
+if (all_classes_integrated$P_qvalue_RNA[i] < 0.05 && all_classes_integrated$P_qvalue[i] < 0.05 && sign(all_classes_integrated$P_est.mean_RNA[i]) == sign(all_classes_integrated$P_est.mean[i])){
+
+	all_classes_integrated$div_category[i] <- "AccD_ExpD_S"
+
+} else if (all_classes_integrated$P_qvalue_RNA[i] < 0.05 && all_classes_integrated$P_qvalue[i] < 0.05 && sign(all_classes_integrated$P_est.mean_RNA[i]) != sign(all_classes_integrated$P_est.mean[i])){
+
+	all_classes_integrated$div_category[i] <- "AccD_ExpD_O"
+
+} else if (all_classes_integrated$P_qvalue_RNA[i] > 0.05 && all_classes_integrated$P_qvalue[i] < 0.05){
+
+	all_classes_integrated$div_category[i] <- "AccD_ExpC"
+
+} else if (all_classes_integrated$P_qvalue_RNA[i] < 0.05 && all_classes_integrated$P_qvalue[i] > 0.05){
+
+	all_classes_integrated$div_category[i] <- "AccC_ExpD"
+
+} else if (all_classes_integrated$P_qvalue_RNA[i] > 0.05 && all_classes_integrated$P_qvalue[i] > 0.05){
+
+  all_classes_integrated$div_category[i] <- "AccC_ExpC"
+
+}
+}
+
+all_classes_integrated$distance_to_exon1 <- abs(all_classes_integrated[,18] - all_classes_integrated[,35])
+
+ggplot(all_classes_integrated, aes(x=div_category, fill=div_category, y=perc_cis_RNA)) +
+geom_boxplot(notch=TRUE)
+
+ggplot(all_classes_integrated, aes(x=div_category, fill=div_category, y=abs(P_est.mean_RNA))) +
+geom_boxplot(notch=TRUE) +
+ylim(0,2)
+
+all_classes_integrated[(all_classes_integrated$class == "inter" | all_classes_integrated$class == "intra") & all_classes_integrated$distance_to_exon1 < 10000,] %>%
+ggplot(aes(x=div_category, fill=div_category, y=distance_to_exon1)) +
+geom_boxplot(notch=TRUE) +
+ylim(0,2)
 
 # calculate basic metrics with integrated matrix
 ## distance to first exon
