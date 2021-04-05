f7b145899ddb45817b03798ed86382c63c2701fa
Henry Ertl <hertl@umich.edu>
2021-04-05 12:48:04 -0400

    z30 plots

--- AS_ATAC_RNA_2020_10_1/Downstream_analyses_and_figures/Z30_weirdness.R
+++ AS_ATAC_RNA_2020_10_1/Downstream_analyses_and_figures/Z30_weirdness.R
@@ -55,6 +55,16 @@ ylab("Estimted accessibility divergence") +
 xlab("") +
 scale_fill_discrete(guide=FALSE)
 
+# look atperc cis per chrom
+G <- ZHR_Z30_regions_ALL_SNPs_GRh_final %>%
+
+ggplot(aes(x=chrom, y=perc_cis, fill=chrom))+
+geom_boxplot(notch=T) +
+theme_main() +
+ylab("Percent cis") +
+xlab("") +
+scale_fill_discrete(guide=FALSE)
+
 
 F <- ZHR_Z30_regions_ALL_SNPs_GRh_final %>%
 
@@ -91,3 +101,107 @@ snps_chrom_count %>%
 ggplot(aes(x=chrom, y=ratio)) +
 geom_col() +
 ylab("snps/chr size")
+## yes
+
+# look at just auto and X
+snps_chrom_count$Auto_X <- "NA"
+
+for (i in 1:nrow(snps_chrom_count)) {
+
+if (snps_chrom_count$chrom[i] == "chrX"){
+
+	snps_chrom_count$Auto_X[i] <- "X"
+
+} else if (snps_chrom_count$chrom[i] == "chr2L"){
+
+	snps_chrom_count$Auto_X[i] <- "Autosome"
+
+} else if (snps_chrom_count$chrom[i] == "chr2R"){
+
+	snps_chrom_count$Auto_X[i] <- "Autosome"
+
+} else if (snps_chrom_count$chrom[i] == "chr3L"){
+
+	snps_chrom_count$Auto_X[i] <- "Autosome"
+
+} else if (snps_chrom_count$chrom[i] == "chr3R"){
+
+	snps_chrom_count$Auto_X[i] <- "Autosome"
+
+} else if (snps_chrom_count$chrom[i] == "chr4"){
+
+snps_chrom_count$Auto_X[i] <- "Autosome"
+
+}
+}
+
+snps_chrom_count %>%
+
+ggplot(aes(x=Auto_X, y=ratio)) +
+geom_boxplot() +
+ylab("snps/chr size")
+## clumping together autosomes is deceptive -- it masks th chr2L effect
+
+
+
+# check gene expression
+
+all_classes_integrated <- read.delim("./AS_ATAC_RNA_2020_10_1/ATAC_RNA_comp/ZHR_Z30_ATAC_RNA_integrated_minimal.txt", header = T)
+
+cis_trans_RNA_CPM <- all_classes_integrated  %>%
+
+ggplot(aes(x = P_est.mean_RNA, y = H_est.mean_RNA)) +
+geom_point(alpha = 0.3) +
+geom_bin2d(bins = 60) +
+geom_hline(yintercept = 0, linetype = "dashed") +
+geom_abline(intercept = 0, slope = 1, linetype = "dashed") + theme_main() + geom_vline(xintercept = 0, linetype = "dashed") + xlim(-2, 2) + ylim(-2, 2) +
+facet_wrap(~chrom)
+
+M <- all_classes_integrated %>%
+
+ggplot(aes(x=chrom, y=abs(P_est.mean_RNA), fill=chrom))+
+geom_boxplot(notch=T) +
+theme_main() +
+ylab("Estimated expression divergence") +
+xlab("") +
+scale_fill_discrete(guide=FALSE)
+
+N <- all_classes_integrated %>%
+
+ggplot(aes(x=chrom, y=perc_cis, fill=chrom))+
+geom_boxplot(notch=T) +
+theme_main() +
+ylab("Percent cis expression") +
+xlab("") +
+scale_fill_discrete(guide=FALSE)
+
+## also the same for gene expression
+
+# try to find differences between regions in epicenter of difference vs outside on chr2L
+# assign in and out
+ZHR_Z30_regions_ALL_SNPs_GRh_final$in_out <- "NA"
+
+for (i in 1:nrow(ZHR_Z30_regions_ALL_SNPs_GRh_final)) {
+
+ifelse (ZHR_Z30_regions_ALL_SNPs_GRh_final$P_est.mean[i] > -1 & ZHR_Z30_regions_ALL_SNPs_GRh_final$P_est.mean[i] < 0 & ZHR_Z30_regions_ALL_SNPs_GRh_final$H_est.mean[i] > -1.25 & ZHR_Z30_regions_ALL_SNPs_GRh_final$H_est.mean[i] < -0.25, ZHR_Z30_regions_ALL_SNPs_GRh_final$in_out[i] <- "in", ZHR_Z30_regions_ALL_SNPs_GRh_final$in_out[i] <- "out")
+
+
+}
+
+# visualize the two groups
+
+cis_trans_ATAC_CPM_chrom <- ZHR_Z30_regions_ALL_SNPs_GRh_final[ZHR_Z30_regions_ALL_SNPs_GRh_final$chrom == "chr2L",]  %>%
+
+ggplot(aes(x = P_est.mean, y = H_est.mean)) +
+geom_point(alpha = 0.3) +
+#geom_bin2d(bins = 50) +
+geom_hline(yintercept = 0, linetype = "dashed") +
+geom_abline(intercept = 0, slope = 1, linetype = "dashed") + theme_main() + geom_vline(xintercept = 0, linetype = "dashed") + xlim(-2, 2) + ylim(-2, 2) +
+facet_wrap(~in_out)
+
+#look at location
+ZHR_Z30_regions_ALL_SNPs_GRh_final[ZHR_Z30_regions_ALL_SNPs_GRh_final$chrom == "chr2L",]  %>%
+ggplot(aes(x=start, fill = in_out)) +
+geom_density() +
+facet_wrap(~in_out) +
+scale_fill_discrete(guide=FALSE)
